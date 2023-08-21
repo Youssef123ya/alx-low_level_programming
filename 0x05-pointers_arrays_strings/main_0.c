90e1ec6ea86cda794175f1906482170656631360
Youssef123ya <joyacoup@gmail.com>
2023-08-21 11:39:12 -0700

    Arrays are not pointers 1

--- 0x05-pointers_arrays_strings/main_0.c
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -1,18 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+#include <unistd.h>
 
-void print_array(int *, int);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  int array[5] = {
-    -198, 298, 402, -1024, 98
-  };
-
-  print_array(array, 5);
-  return (0);
-}
+#endif
