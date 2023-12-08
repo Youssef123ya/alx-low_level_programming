26056c20d4504090bcdf61e9bfc21cf28701fba2
Youssef123ya <joyacoup@gmail.com>
2023-12-07 16:08:35 -0800

    doneee

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -10,25 +10,25 @@
  */
 int main(void)
 {
-   dlistint_t *head; // Declare a pointer to the head of the doubly linked list
-   dlistint_t *new; // Declare a pointer to a new node
-   dlistint_t hello = {8, NULL, NULL}; // Create a node with value 8 and NULL next and prev pointers
-   size_t n; // Declare a variable to hold the number of elements in the list
+    dlistint_t *head;
+    dlistint_t *new;
+    dlistint_t hello = {8, NULL, NULL};
+    size_t n;
 
-   head = &hello; // Set the head of the list to the 'hello' node
-   new = malloc(sizeof(dlistint_t)); // Allocate memory for a new node
-   if (new == NULL) // Check if the memory allocation was successful
-   {
-       dprintf(2, "Error: Can't malloc\n"); // If not, print an error message and return failure
-       return (EXIT_FAILURE);
-   }
-   new->n = 9; // Set the value of the new node to 9
-   head->prev = new; // Set the prev pointer of the 'hello' node to the new node
-   new->next = head; // Set the next pointer of the new node to the 'hello' node
-   new->prev = NULL; // Set the prev pointer of the new node to NULL (since it's the first node)
-   head = new; // Set the head of the list to the new node
-   n = print_dlistint(head); // Print the list and get the number of elements
-   printf("-> %lu elements\n", n); // Print the number of elements
-   free(new); // Free the memory allocated for the new node
-   return (EXIT_SUCCESS); // Return success
+    head = &hello;
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        dprintf(2, "Error: Can't malloc\n");
+        return (EXIT_FAILURE);
+    }
+    new->n = 9;
+    head->prev = new;
+    new->next = head;
+    new->prev = NULL;
+    head = new;
+    n = print_dlistint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (EXIT_SUCCESS);
 }
--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,25 +9,22 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-   int count; // Variable to keep track of the number of nodes
+	int count;
 
-   count = 0; // Initialize count to 0
+	count = 0;
 
-   // If the head of the list is NULL, return count (which is 0)
-   if (h == NULL)
-       return (count);
+	if (h == NULL)
+		return (count);
 
-   // Traverse to the start of the list
-   while (h->prev != NULL)
-       h = h->prev;
+	while (h->prev != NULL)
+		h = h->prev;
 
-   // Traverse the list and print each node
-   while (h != NULL)
-   {
-       printf("%d\n", h->n); // Print the value of the current node
-       count++; // Increment the count
-       h = h->next; // Move to the next node
-   }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
 
-   return (count); // Return the count
+	return (count);
 }
